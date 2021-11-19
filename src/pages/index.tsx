65348dfa4ac44e2aa29557d9b7dc0df2700ee6fc
André Tashiro <andre_seichi@hotmail.com>
2021-11-19 17:59:27 -0300

    página home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,14 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+
+import { FiCalendar, FiUser } from 'react-icons/fi';
+
+import Prismic from '@prismicio/client';
+import { useState } from 'react';
+
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,13 +34,126 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const postsFormated = postsPagination.results.map(post => {
+    return {
+      uid: post.uid,
+      data: post.data,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM yyyy',
+        { locale: ptBR }
+      ),
+    };
+  });
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+  const [posts, setPosts] = useState<Post[]>(postsFormated);
+  const [nextPage, setNextPage] = useState(postsPagination.next_page);
 
-//   // TODO
-// };
+  async function handleLoadMorePosts(): Promise<void> {
+    const morePosts = await fetch(`${nextPage}`).then(response =>
+      response.json()
+    );
+
+    const morePostsFormated: Post[] = morePosts.results.map(post => {
+      return {
+        uid: post.uid,
+        first_publication_date: format(
+          new Date(post.first_publication_date),
+          'dd MMM yyyy',
+          { locale: ptBR }
+        ),
+        data: post.data,
+      };
+    });
+
+    setPosts([...posts, ...morePostsFormated]);
+    setNextPage(morePosts.next_page);
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <main className={commonStyles.container}>
+        <div className={commonStyles.content}>
+          {posts.map(post => (
+            <div className={styles.post} key={post.uid}>
+              <Link href={`/post/${post.uid}`}>
+                <a>
+                  <h1 className={styles.title}>{post.data.title}</h1>
+                  <h2 className={styles.subtitle}>{post.data.subtitle}</h2>
+                  <div className={styles.footer}>
+                    <time className={styles.time}>
+                      <FiCalendar />
+                      <span className={styles.publicationDate}>
+                        {post.first_publication_date}
+                      </span>
+                    </time>
+                    <p className={styles.author}>
+                      <FiUser />
+                      <span className={styles.postAuthor}>
+                        {post.data.author}
+                      </span>
+                    </p>
+                  </div>
+                </a>
+              </Link>
+            </div>
+          ))}
+
+          {nextPage && (
+            <button
+              type="button"
+              onClick={handleLoadMorePosts}
+              className={styles.button}
+            >
+              Carregar mais posts
+            </button>
+          )}
+        </div>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      fetch: [
+        'posts.slug',
+        'posts.title',
+        'posts.subtitle',
+        'posts.author',
+        'posts.banner',
+        'posts.content',
+      ],
+      pageSize: 1,
+    }
+  );
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  return {
+    props: {
+      postsPagination: {
+        results: posts,
+        next_page: postsResponse.next_page,
+      },
+    },
+  };
+};
